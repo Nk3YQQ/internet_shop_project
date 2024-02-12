db44084bbaf70e66b09943d0d7491d75a3f56777
egor <ega.nik5674@gmail.com>
2024-02-12 22:08:12 +0300

    Init path.py

---
+++ catalog/path.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+REDMIBOOK_14_PATH = Path(__file__).parent.joinpath("static", "images", "xiaomi-redmibook-14.jpg")
+REDMIBOOK_16_PATH = Path(__file__).parent.joinpath("static", "images", "xiaomi-redmibook-16.jpg")
