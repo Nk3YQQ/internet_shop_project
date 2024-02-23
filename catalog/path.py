81c2e19d50a15994a104e15e4384d96c757e08f6
egor <ega.nik5674@gmail.com>
2024-02-23 14:17:08 +0300

    Update catalog/path.py

--- catalog/path.py
+++ catalog/path.py
@@ -2,3 +2,5 @@ from pathlib import Path
 
 REDMIBOOK_14_PATH = Path(__file__).parent.joinpath("static", "images", "xiaomi-redmibook-14.jpg")
 REDMIBOOK_16_PATH = Path(__file__).parent.joinpath("static", "images", "xiaomi-redmibook-16.jpg")
+PRODUCTS_DATA_PATH = Path(__file__).parent.parent.joinpath("fixtures", "catalog", "products.json")
+CATEGORIES_DATA_PATH = Path(__file__).parent.parent.joinpath("fixtures", "catalog", "categories.json")
